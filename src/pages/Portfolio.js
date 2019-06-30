31f05de7620b4af93226ee96f83da0e0c59bee35
Emroze <emrozesd@users.noreply.github.com>
2019-06-29 19:08:02 -0700

    added all portfolio screen shots

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -3,7 +3,7 @@ import { Row, Col } from "react-bootstrap"
 import PortfolioContainer from "../components/PortfolioContainer"
 import PortfolioModal from "../components/PortfolioModal";
 import PortfolioImage from "../components/PortfolioImage"
-import blockPreview from "../assets/page-images/block-preview.png";
+import blockPreview from "../assets/page-images/block-party-preview.png";
 import blockScreenOne from "../assets/page-images/block-party-shot-one.png";
 import blockScreenTwo from "../assets/page-images/block-party-shot-two.png";
 import campPreview from "../assets/page-images/camp-wanna-preview.png";
@@ -11,6 +11,8 @@ import readingPreview from "../assets/page-images/reading-preview.png";
 import readingScreenOne from "../assets/page-images/reading-shot-one.png"
 import readingScreenTwo from "../assets/page-images/reading-shot-two.png"
 import friendPreview from "../assets/page-images/friend-preview.png";
+import friendScreenOne from "../assets/page-images/friend-shot-one/png"
+import friendScreenTwo from "../assets/page-images/friend-shot-two.png"
 import brewsPreview from "../assets/page-images/brews-preview.png";
 import mongoPreview from "../assets/page-images/mongo-preview.png";
 import projects from "../projects.json";
@@ -38,6 +40,7 @@ class Portfolio extends Component {
                     />
                     </PortfolioImage>
                 </Col>
+
                 <Col xs={12} md={6} lg={4}>
                   <PortfolioImage
                     img={readingPreview}
@@ -54,30 +57,77 @@ class Portfolio extends Component {
                     />
                   </PortfolioImage>
                 </Col>
+
                 <Col xs={12} md={6} lg={4}>
-                <PortfolioImage
-                    img={friendPreview}
-                />
+                  <PortfolioImage
+                      img={friendPreview}
+                      alt="Friend Finder Preview"
+                  >
+                  Friend Finder
+                  <PortfolioModal
+                      title={projects[2].title}
+                      description={projects[2].description}
+                      tagline={projects[2].tagline}
+                      imageOne={friendScreenOne}
+                      imageTwo={friendScreenTwo}
+                      techsUsed={projects[2].techsUsed}
+                    />
+                  </PortfolioImage>
                 </Col>
 
-              <Col xs={12} md={6} lg={4}>
+                <Col xs={12} md={6} lg={4}>
                     <PortfolioImage
                         img={campPreview}
-                        alt="block-party-preview"
+                        alt="Camp Wanna Preview"
                     >
-                        Block Party
-                        <PortfolioModal/>
+                        Camp Wannagetajob
+                    <PortfolioModal
+                        title={projects[3].title}
+                        description={projects[3].description}
+                        tagline={projects[3].tagline}
+                        imageOne={friendScreenOne}
+                        imageTwo={friendScreenTwo}
+                        techsUsed={projects[3].techsUsed}
+                        
+                      />
+                    </PortfolioImage>
+
+                </Col>
+
+                <Col xs={12} md={6} lg={4}>
+                    <PortfolioImage
+                        img={brewsPreview}
+                        alt="Brews Beats Preview"
+                    >
+                      Brews and Beats
+                    <PortfolioModal
+                      title={projects[4].title}
+                      description={projects[4].description}
+                      tagline={projects[4].tagline}
+                      imageOne={friendScreenOne}
+                      imageTwo={friendScreenTwo}
+                      techsUsed={projects[4].techsUsed}
+                      
+                    />
                     </PortfolioImage>
                 </Col>
+
                 <Col xs={12} md={6} lg={4}>
-                <PortfolioImage
-                    img={brewsPreview}
-                />
-                </Col>
-                <Col xs={12} md={6} lg={4}>
-                <PortfolioImage
-                    img={mongoPreview}
-                />
+                    <PortfolioImage
+                        img={mongoPreview}
+                        alt="News Scraper Preview"
+                    >
+                      News Scraper
+                    <PortfolioModal
+                      title={projects[5].title}
+                      description={projects[5].description}
+                      tagline={projects[5].tagline}
+                      imageOne={friendScreenOne}
+                      imageTwo={friendScreenTwo}
+                      techsUsed={projects[5].techsUsed}
+                      
+                    />
+                    </PortfolioImage>
                 </Col>
             </Row>
         </PortfolioContainer>
