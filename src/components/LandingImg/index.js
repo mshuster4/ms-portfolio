60e69d86ca4e3cff6ac7cce899f2ff20bb32b860
Emroze <emrozesd@users.noreply.github.com>
2019-07-09 20:54:09 -0700

    added background image for smaller screen

--- src/components/LandingImg/index.js
+++ src/components/LandingImg/index.js
@@ -1,11 +1,25 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import LandingImgLg from "../../assets/page-images/landing-img-lg.png";
 import LandingImgSm from "../../assets/page-images/landing-img-sm.png";
 import "./style.css"
 
 function LandingImg (props) {
 
-    const imageURL = LandingImgLg;    
+    const [ setWindowWidth ] = useState(window.innerWidth);
+
+    const imageURL = window.innerWidth >= 568 ? LandingImgLg : LandingImgSm;
+
+     const handleWindowResize = () => {
+        setWindowWidth(window.innerWidth);
+    };
+
+     useEffect(() => {
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        }
+    });
 
     return (
        <div className="landing-img" style={{ backgroundImage: `url(${imageURL})` }}>
